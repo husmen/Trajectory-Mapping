041de29a87a42b4cf219cfa770f4837d33441f29
husmen <husmen93@gmail.com>
2018-03-05 13:18:33 +0300

    sending files

--- client/client-app.py
+++ client/client-app.py
@@ -2,22 +2,41 @@
 
 import socket
 
+# Global declarations
+#TCP_IP = 'localhost'
+TCP_HOST = socket.gethostname() # get local machine name
+TCP_PORT = 9999 # set port
+BUFFER_SIZE = 1024 # set 1024 bytes as buffer size
+FILE_FULL = 'original_file.txt'
+FILE_REDUCED = 'new_file.txt'
+
 # create a socket object
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-
-# get local machine name
-host = socket.gethostname()                           
-
-port = 9999
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # connection to hostname on the port.
-s.connect((host, port))
+s.connect((TCP_HOST, TCP_PORT))
 
-msgToSend = 'Houssem' 
-s.send(msgToSend.encode('ascii'))                             
+# send message to server
+msgToSend = 'Houssem'
+s.send(msgToSend.encode('ascii'))
 
-# Receive no more than 1024 bytes
-msg = s.recv(1024)                                     
+# Receive no more than BUFFER_SIZE bytes
+msg = s.recv(BUFFER_SIZE)
+
+# print received reply
+print(msg.decode('ascii'))
+
+# sending a file
+filename = FILE_FULL
+f = open(filename, 'rb')
+l = f.read(BUFFER_SIZE)
+while (l):
+    s.send(l)
+    print('Sent ', repr(l))
+    l = f.read(BUFFER_SIZE)
+f.close()
+print("\n*** FILE SENDING COMPLETE ***\n")
+
+
 
 s.close()
-print (msg.decode('ascii'))
--- server/server-app.py
+++ server/server-app.py
@@ -5,22 +5,24 @@ import time
 import sys
 import os
 
+# Global declarations
+#TCP_IP = 'localhost'
+TCP_HOST = socket.gethostname() # get local machine name
+TCP_PORT = 9999 # set port
+BUFFER_SIZE = 1024 # set 1024 bytes as buffer size
+FILE_FULL = 'original_file.txt'
+FILE_REDUCED = 'new_file.txt'
+
 exitFlag = False
 
 # create a socket object
-serversocket = socket.socket(
-	        socket.AF_INET, socket.SOCK_STREAM)
-
-# get local machine name
-host = socket.gethostname()
-
-port = 9999
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # bind to the port
-serversocket.bind((host, port))
+serverSocket.bind((TCP_HOST, TCP_PORT))
 
 # queue up to 5 requests
-serversocket.listen(5)
+serverSocket.listen(5)
 
 
 class serverThread(threading.Thread):
@@ -30,22 +32,37 @@ class serverThread(threading.Thread):
         self.name = name
 
     def run(self):
-        print("Starting server routine thread")
+        print("\n*** STARTING SERVER ROUTINE THREAD ***\n")
         while True:
             # establish a connection
-            clientsocket, addr = serversocket.accept()
+            clientSocket, addr = serverSocket.accept()
             currentTime = time.ctime(time.time()) + "\r\n"
 
             # receive message from client
-            msg = clientsocket.recv(1024)
+            msg = clientSocket.recv(1024)
 
             print("Got a connection request by {} from {} at {}".format(
                 msg.decode('ascii'), str(addr), currentTime))
 
             msgToSend = 'Thank you for connecting, {}'.format(
                 msg.decode('ascii')) + "\r\n"
-            clientsocket.send(msgToSend.encode('ascii'))
-            clientsocket.close()
+            clientSocket.send(msgToSend.encode('ascii'))
+
+            with open(FILE_FULL, 'wb') as f:
+                print('file opened')
+                while True:
+                    print('receiving data...')
+                    data = clientSocket.recv(1024)
+                    print('data=%s', (data))
+                    if not data:
+                        break
+                    # write data to a file
+                    f.write(data)
+
+            f.close()
+            print('Successfully get the file')
+
+            clientSocket.close()
 
 class inputThread(threading.Thread):
     def __init__(self, threadID, name):
@@ -53,14 +70,17 @@ class inputThread(threading.Thread):
         self.threadID = threadID
         self.name = name
     def run(self):
-        print("Starting input routine thread")
+        print("\n*** STARTING INPUT ROUTINE THREAD ***\n")
         flag = True
         while flag:
-            inputCMD = input("your command: ")
-            print(inputCMD)
-            if str(inputCMD) == "0":
+            try:
+                inputCMD = input("your command: ")
+                #print(inputCMD)
+                if str(inputCMD) == "0":
+                    flag = False
+                    #exitFlag = True
+            except:
                 flag = False
-                #exitFlag = True
             else:
                 pass
         print("stop command received\n")        
@@ -78,6 +98,6 @@ while inputRoutine.isAlive():
 
 # wait for threads to end
 serverRoutine.join(1)
-print("program exiting\n")
+print("\n*** PROGRAM EXITING ***\n")
 #sys.exit(0)
 os._exit(0)
